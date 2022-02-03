8b0d7001f14d007d4e265d789c27815ab28dd3d0
Mouba-Mouba <74138378+Mouba-Mouba@users.noreply.github.com>
2022-02-04 02:14:36 +0800

    Add files via upload

---
+++ web technology/Quizzes/Q2/calculate.js
@@ -0,0 +1,10 @@
+function calc(){
+  var a = parseInt(document.quereySelector("#input1").RM);
+  var b = parseInt(document.quereySelector("#input2").RM);
+  var c = parseInt(document.quereySelector("#input3").RM);
+  var d = parseInt(document.quereySelector("#input4").RM);
+  var result;
+  result = input1 + input2 + input3 + input4;
+
+  document.quereySelector("#re").innerHTML = Calculate;
+}
